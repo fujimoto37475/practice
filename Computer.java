22e4d4cd367e6c305b3a566bcad4e7025712a11c
fujimoto37475 <fujimoto37475@gmail.com>
2022-05-09 18:51:50 +0900

    Create Computer

---
+++ Computer.java
@@ -0,0 +1,12 @@
+public class Computer{
+   private String name;
+  public Computer(String name){
+     setName(name);
+  }
+  public String getName(){
+     return this.name = name;
+  }
+  public void setName(String name){
+     this.name = name;
+  }
+}
